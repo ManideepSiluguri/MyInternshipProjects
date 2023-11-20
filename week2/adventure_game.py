8b2336c07852ccff268154ac3808831c0e2520ab
ManideepSiluguri <137205630+ManideepSiluguri@users.noreply.github.com>
2023-11-20 19:53:10 +0530

    adventure_game.py

--- week2/adventure_game.py
+++ week2/adventure_game.py
@@ -1,86 +1,105 @@
-def leftpath():
-    print("you chose left path,you discover a hidden lake with a mysterious aura.")
-    print("choices:\n1.Swim across the lake\n2.Follow the path around the lake")
-    quest11 = int(input())
-    if quest11 == 1:
-        swim_across()
-    else:
-        continue_on_your_own()
-
-def swim_across():
-    print("you chose to swim across the lake, you encounter friendly water spirits.")
-    print("choices:\n1.Accept their guidance.\n2.Politely decline and continue on your own.")
-    quest111 = int(input())
-    if quest111 == 1:
-        accept_guidance()
-    else:
-        continue_on_your_own()
-def accept_guidance():
-    print("you chose to accepth their guidance,The water spirits guide you to a magical portal.")
-    print("choices:\n1.enter the portal\n2.thank the spirits and continue your journey")
-    quest1111 = int(input())
-    if quest1111 == 1:
-        enter_portal()
-    else:
-        continue_on_your_own()
-def enter_portal():
-    print("The portal transports you to the heart of the forest, where you find the ancient treasure.")
-
-def continue_on_your_own():
-    print("you chose to continue on your own,You find a hidden path leading to a troll bridge.")
-    print("choices:\n1.Attempt to cross the bridge.\n2.Find another way around.")
-    quest12 = int(input())
-    if quest12 == 1:
-        cross_bridge()
-    else:
-        rightpath()
-def cross_bridge():
-    print("you chose to cross the troll bridge,The troll demands a toll.")
-    print("choices:\n1.Pay the toll.\n2.Try to outsmart the troll.")
-    quest121 = int(input())
-    if quest121 == 1:
-        print("you paid toll and cross the bridge and theres nothing there")
-    else:
-        print("the troll kills you")
+def forest_path():
+    print("Welcome to the Enchanted Forest Adventure!")
+    print("You find yourself at the entrance of a mystical forest.")
+    print("As you step in, the path splits into two.")
+    print("Choices:\n1. Take the glowing path on the left\n2. Explore the shadowy path on the right")
+    choice = int(input())
     
-
-def rightpath():
-    print(" you encounter a fierce dragon.")
-    print("choices:\n1.Convince the dragon you mean no harm.\n2.Fight the dragon.")
-    quest2 = int(input())
-    if quest2 == 1:
-        convince_dragon()
+    if choice == 1:
+        glowing_path()
     else:
-        fight_dragon()
-def convince_dragon():
-    print("The dragon, impressed by your bravery, allows you to pass safely.")
-def fight_dragon():
-    print("you are fighting dragon")
-    print("choices:\n1.use your sword\n2.attempt to escape ")
-    quest21 = int(input())
-    if quest21 == 1:
-        use_sword()
+        shadowy_path()
+
+def glowing_path():
+    print("You chose the glowing path and encounter fireflies lighting up the way.")
+    print("Choices:\n1. Follow the fireflies deeper into the forest\n2. Investigate a strange noise nearby")
+    choice = int(input())
+
+    if choice == 1:
+        follow_fireflies()
     else:
-        escape()
-def use_sword():
-    print("If you choose to fight, you defeat the dragon and proceed and you find a hidden tunnel that leads to the treasure.")
-    print("you found the tresure and you win the game")
-def escape():
-    print("the dragon catches you and you die")
+        investigate_noise()
 
-print("Welcome to the Text-based adventure game")
-print("-----------------------------------------------------")
-print("GAME INTRODUCTION: ")
-print("You, the brave adventurer, find yourself standing at the edge of the Enchanted Forest. Legends speak of hidden treasures and dangerous creatures lurking within its depths. Your quest is to navigate through the forest, make crucial decisions, and ultimately uncover the long-lost treasure guarded by ancient magic.")
-print()
-print("You start your journey at the forest entrance with only a map and a rusty sword.")
-print()
-print("s you venture into the forest, you come across a fork in the road.")
-print("you have to choose between theese 2 choices")
-print("1.Take the left path\n2.Take the right path")
-quest1 = int(input())
-if quest1 == 1:
-    leftpath()
-else:
-    rightpath()
+def follow_fireflies():
+    print("The fireflies lead you to a magical meadow.")
+    print("Choices:\n1. Dance with the fairies in the meadow\n2. Continue exploring the forest")
+    choice = int(input())
 
+    if choice == 1:
+        dance_with_fairies()
+    else:
+        continue_exploring()
+
+def dance_with_fairies():
+    print("You join the fairies in a joyful dance, and they reward you with a magic amulet.")
+    print("The amulet shimmers with mysterious energy.")
+
+def investigate_noise():
+    print("You investigate the noise and discover a lost unicorn.")
+    print("Choices:\n1. Help the unicorn find its way home\n2. Leave the unicorn and explore further")
+    choice = int(input())
+
+    if choice == 1:
+        help_unicorn()
+    else:
+        continue_exploring()
+
+def help_unicorn():
+    print("You help the unicorn find its way home, and it grants you a ride to a hidden grove.")
+    print("In the grove, you find a secret passage to a treasure chamber.")
+
+def continue_exploring():
+    print("You continue exploring the forest and come across a mysterious old tree.")
+    print("Choices:\n1. Talk to the tree\n2. Climb the tree for a better view")
+    choice = int(input())
+
+    if choice == 1:
+        talk_to_tree()
+    else:
+        climb_tree()
+
+def talk_to_tree():
+    print("The old tree shares ancient wisdom and points you to the path of enlightenment.")
+
+def climb_tree():
+    print("As you climb the tree, you spot a hidden path that leads to a magical waterfall.")
+    print("Choices:\n1. Follow the path to the waterfall\n2. Return to the forest trail")
+    choice = int(input())
+
+    if choice == 1:
+        waterfall_path()
+    else:
+        forest_path()
+
+def waterfall_path():
+    print("You follow the path and discover a breathtaking waterfall with a mysterious aura.")
+    print("Choices:\n1. Dive into the waterfall\n2. Examine the surroundings before deciding")
+    choice = int(input())
+
+    if choice == 1:
+        dive_into_waterfall()
+    else:
+        examine_surroundings()
+
+def dive_into_waterfall():
+    print("You take a leap of faith into the waterfall and find yourself in a hidden underwater cavern.")
+    print("In the cavern, you uncover an ancient chest filled with treasure.")
+    print("Congratulations! You've completed the Enchanted Forest Adventure and found the hidden treasure.")
+
+def examine_surroundings():
+    print("You carefully examine the surroundings and discover a hidden passage behind the waterfall.")
+    print("Choices:\n1. Enter the hidden passage\n2. Return to the forest trail")
+    choice = int(input())
+
+    if choice == 1:
+        hidden_passage()
+    else:
+        forest_path()
+
+def hidden_passage():
+    print("You enter the hidden passage and find yourself in an underground chamber.")
+    print("In the chamber, you encounter a guardian spirit who grants you a magical blessing.")
+    print("Congratulations! You've completed the Enchanted Forest Adventure.")
+
+# Start the game
+forest_path()
